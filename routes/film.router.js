0ca0faefecb20a6a973656c90f50476acfc79a32
alexseik <alexseik@hotmail.com>
2020-05-07 13:06:23 +0200

    Add update method to film router

--- routes/film.router.js
+++ routes/film.router.js
@@ -11,27 +11,47 @@ class FilmRouter {
     }
 
     static async getById(ctx) {
-        
+        logger.info(`Obtaining film with id ${ctx.params.id}`);
+        const film = films.find((f) => f.id === +ctx.params.id);
+        if (!film) {
+            ctx.throw(404, 'Film not found');
+            // return;
+        }
+        ctx.body = film;
     }
 
     static async create(ctx) {
         logger.info(`Creating new film with body ${ctx.request.body}`);
         const film = ctx.request.body;
-        film.id = nextId++;films.push(film);
+        film.id = nextId++;
+        films.push(film);
         ctx.body = film;
         ctx.status = 201;
     }
 
     static async update(ctx) {
-        
+        logger.info(`Updating film with id ${ctx.params.id}`);
+        let film = null;
+        films = films.map((f) => {
+            if (f.id === +ctx.params.id){
+                film = Object.assign(f, ctx.request.body);
+                return film;
+            }
+            return f;
+        });
+        if (!film) {
+            ctx.throw(404, 'Film not found');
+            return;
+        }
+        ctx.body = film;
     }
 
     static async delete(ctx) {
-        
+
     }
 }
 
-const router = new Router({prefix: '/film'});
+const router = new Router({ prefix: '/film' });
 router.get('/', FilmRouter.get);
 router.get('/:id', FilmRouter.getById);
 router.post('/', FilmRouter.create);
